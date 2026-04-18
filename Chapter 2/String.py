f0683864bf77fcd704f3748312a0d213f419d762
BhartiSahu30 <bhartirdsahu@gmail.com>
2026-04-18 16:23:29 +0530

    string

---
+++ Chapter 2/String.py
@@ -0,0 +1,12 @@
+str1 ="Apna College"
+len =len(str1)
+print(len)
+ch =str1[3]
+print(ch)
+print(str1[1:5] +"\n"+ str1[:4] +"\n"+ str1[5:12])
+"""str2 ="College"
+Final_str = str1 + " "+str2
+print(Final_str)"""
+
+#NEGATIVE INDEXING
+print(str1[-7:-2])
